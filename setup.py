d4ff1fc1a258aacfed7856ab3c12f20028f7772f
Qianshui <qianshui.jiang@intel.com>
2023-12-27 17:32:23 +0800

    re construct setup.py

--- setup.py
+++ setup.py
@@ -8,23 +8,12 @@ os.environ['DISTUTILS_USE_SDK']='1'
 cm_root_path = os.environ['CSDK_IGC']
 file_root_path = os.path.dirname(__file__)
 
-
-LIB_OPENCL = "Intel_OpenCL_ICD64"
-LIB_OPENCL_PATH = os.path.join(cm_root_path, 'runtime/opencl/lib')
-
-LIB_LEVEL0 = "ze_loader"
-LIB_LEVEL0_PATH = os.path.join(cm_root_path, 'runtime/level_zero/lib')
-
+# ----include Path-----
+RT_L0_PATH = os.path.join(file_root_path,'rt_l0')
 PYBIND11_JSON_PATH = os.path.join(file_root_path, '3rdparty/pybind11_json/include')
 NLOHMANN_JSON =  os.path.join(file_root_path, r'3rdparty/json/include')
-RT_L0_PATH = os.path.join(file_root_path,'rt_l0')
 
-# ----include LIB and Path-----
 include_path_list = list()
-library_path_list = list()
-library_list = list()
-src_list = list()
-
 include_path_list.append('.')
 include_path_list.append(RT_L0_PATH)
 include_path_list.append(PYBIND11_JSON_PATH)
@@ -36,20 +25,32 @@ include_path_list.append(os.path.join(cm_root_path, 'runtime/level_zero/include 
 include_path_list.append(os.path.join(cm_root_path, 'usr/include '))
 
 
-# library_path_list.append(LIB_LEVEL0_PATH)
+# ----library Path & List-----
+LIB_OPENCL = "Intel_OpenCL_ICD64"
+LIB_OPENCL_PATH = os.path.join(cm_root_path, 'runtime/opencl/lib')
+LIB_LEVEL0 = "ze_loader"
+LIB_LEVEL0_PATH = os.path.join(cm_root_path, 'runtime/level_zero/lib')
+
+library_path_list = list()
+library_list = list()
+# library_path_list.append(LIB_OPENCL_PATH)
 # library_list.append('Intel_OpenCL_ICD64') # openCL runtime
+
 library_path_list.append(LIB_LEVEL0_PATH)
 library_list.append('ze_loader') # Level 0 runtime
 
+
+# ----source code  List-----
+src_list = list()
 src_list = ['zbench.cpp', 'rt_l0/l0_launch.cpp', 'rt_l0/l0_launch_model_shader.cpp', 'dev_test.cpp']
 
 
 # exit()
 module= CppExtension(name='zbench', 
-                    sources= src_list,
+                    sources=src_list,
                     include_dirs=include_path_list,
                     library_dirs=library_path_list,
-                    libraries= library_list,
+                    libraries=library_list,
                     cxx_std=17,
                     define_macros=[('VERSION_INFO', __version__)])
 
