ed5f06a3d09088dfdacdd6eef8533ab7b9f9ec93
Qianshui <qianshui.jiang@intel.com>
2024-04-22 16:59:02 +0800

    fix the dxdispatch path

--- tests/dev_flash_att/dxdispatch_dump_data_llama2.py
+++ tests/dev_flash_att/dxdispatch_dump_data_llama2.py
@@ -6,7 +6,7 @@ import numpy as np
 np.random.seed(123)
 
 
-dxdispatch = r"D:\dml_workspace\zbench\tests\dev_flash_att\DxDispatchRelWithDebInfo\dxdispatch.exe"
+dxdispatch = r"DxDispatchRelWithDebInfo\dxdispatch.exe"
 
 
 def launch_dxdispatch(input_file):
